928833014583c9641457ff79157735f58c7b3600
xiaoshideta <86964787+xiaoshideta@users.noreply.github.com>
2026-02-03 14:03:37 +0800

    Update eval_dream.sh

--- Dream/eval_dream.sh
+++ Dream/eval_dream.sh
@@ -1,10 +1,10 @@
-tasks="gsm8k_cot gsm8k_cot"
-nshots="5 5"
-lengths="256 512"
-temperatures="0 0"
-steps="8 16"
-window_size="32 32"
-confidence_alpha="0.3 0.3"
+tasks="mbpp mbpp gsm8k_cot gsm8k_cot minerva_math minerva_math"
+nshots="3 3 5 5 4 4"
+lengths="256 512 256 512 256 512"
+temperatures="0 0 0 0 0 0"
+steps="8 16 8 16 8 16"
+window_size="192 192 32 32 32 32"
+confidence_alpha="0.3 0.6 0.3 0.3 0.1 0.3"
 model=/root/autodl-tmp/model/Dream-base
 # Create arrays from space-separated strings
 read -ra TASKS_ARRAY <<< "$tasks"
@@ -22,7 +22,7 @@ for i in "${!TASKS_ARRAY[@]}"; do
     output_path=evals_results1/${TASKS_ARRAY[$i]}-ns${NSHOTS_ARRAY[$i]}
     echo "Task: ${TASKS_ARRAY[$i]}, Shots: ${NSHOTS_ARRAY[$i]}; Output: $output_path"
     accelerate launch eval.py --model dream \
-        --model_args pretrained=${model},max_new_tokens=${LENGTH_ARRAY[$i]},diffusion_steps=${TEMP_NUM[$i]},add_bos_token=true,temperature=${TEMP_ARRAY[$i]},top_p=0.95,alg=confidence_threshold,threshold=0.9,use_cache=true,window_size=${WIN_SIZE[$i]},confidence_alpha=${COM_ALPHA[$i]} \
+        --model_args pretrained=${model},max_new_tokens=${LENGTH_ARRAY[$i]},diffusion_steps=${TEMP_NUM[$i]},add_bos_token=true,temperature=${TEMP_ARRAY[$i]},alg=confidence_threshold,threshold=0.9,use_cache=true,window_size=${WIN_SIZE[$i]},confidence_alpha=${COM_ALPHA[$i]} \
         --tasks ${TASKS_ARRAY[$i]} \
         --num_fewshot ${NSHOTS_ARRAY[$i]} \
         --batch_size 1 \
